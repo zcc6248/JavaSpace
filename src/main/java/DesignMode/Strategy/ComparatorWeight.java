a4b105fa034c4692df76442c4766b7f67faadae5
zcc6248 <80239128+zcc6248@users.noreply.github.com>
2021-04-16 11:14:56 +0800

    Delete src/main/java/DesignMode/Strategy directory

--- src/main/java/DesignMode/Strategy/ComparatorWeight.java
+++
@@ -1,8 +0,0 @@
-package DesignMode.Strategy;
-
-public class ComparatorWeight implements IComparator<Strategy_dog> {
-    @Override
-    public int compare(Strategy_dog o1, Strategy_dog o2) {
-        return Integer.compare(o1.weight, o2.weight);
-    }
-}
--- src/main/java/DesignMode/Strategy/IComparator.java
+++
@@ -1,5 +0,0 @@
-package DesignMode.Strategy;
-
-public interface IComparator<T> {
-    int compare(T o1, T o2);
-}
