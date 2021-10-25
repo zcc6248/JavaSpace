434febebdcd151dd1ded25d9946861b76700b581
zcc6248 <624875431@qq.com>
2021-10-25 16:58:58 +0800

    RPC案例

--- src/main/java/RPCTest/MyRPC.java
+++ src/main/java/RPCTest/MyRPC.java
@@ -4,7 +4,6 @@ import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.PooledByteBufAllocator;
-import io.netty.buffer.Unpooled;
 import io.netty.buffer.UnpooledByteBufAllocator;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -24,22 +23,22 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class MyRPC {
 
-    private static int client_num = 50;
-    private static int LoopGroup_num = 10;
+    private static final int CLIENT_NUM = 50;
+    private static final int LOOPGROUP_NUM = 10;
     public static void main(String[] args) {
 
         new Thread(()->{
             serverStart();
         }).start();
         System.out.println("服务器启动...........");
-        Thread[] t = new Thread[client_num];
+        Thread[] t = new Thread[CLIENT_NUM];
         AtomicInteger j = new AtomicInteger(0);
-        for (int i = 0; i < client_num; i++) {
+        for (int i = 0; i < CLIENT_NUM; i++) {
             t[i] = new Thread(()->{
-                car ca = proxyGet(car.class);
-                String arg = "你好官方电话发给" + j.getAndIncrement();
+                Icar ca = proxyGet(Icar.class);
+                String arg = "你好官方电话-0014" + j.getAndIncrement();
                 String hello = ca.hello(arg);
-                System.out.println(hello + "    client:" + arg);
+                System.out.println(hello + "    client: " + arg);
             });
         }
         for (int i = 0; i < t.length; i++) {
@@ -51,19 +50,24 @@ public class MyRPC {
             e.printStackTrace();
         }
 
-        Enumeration<InetSocketAddress> keys = clientFactory.instance.outboxs.keys();
+        Enumeration<InetSocketAddress> keys = SocketChannelPoolFactory.instance.outboxs.keys();
         Iterator<InetSocketAddress> iterator = keys.asIterator();
         System.out.println("=======================");
         while (iterator.hasNext()){
             InetSocketAddress next = iterator.next();
-            clientPool clientPool = clientFactory.instance.outboxs.get(next);
-            System.out.println(next + "连接池数量: " + clientPool.clients.length);
+            SocketChannelPool clientPool = SocketChannelPoolFactory.instance.outboxs.get(next);
+            System.out.println(next + "连接池数量: " + clientPool.socketchanns.length);
         }
-        System.out.println("requestid重复数量：" + staticUUID.getRepeatNum());
+        System.out.println("requestid重复数量：" + RequestCallback.getRepeatNum());
     }
 
     private static void serverStart(){
-        NioEventLoopGroup eventExecutors = new NioEventLoopGroup(LoopGroup_num);
+        MyCar myCar = new MyCar();
+        MyFly myFly = new MyFly();
+        MappingImpl.addMapping(Icar.class.getName(), myCar);
+        MappingImpl.addMapping(Ifly.class.getName(), myFly);
+
+        NioEventLoopGroup eventExecutors = new NioEventLoopGroup(LOOPGROUP_NUM);
         ServerBootstrap serverBootstrap = new ServerBootstrap();
         ChannelFuture bind = serverBootstrap.group(eventExecutors, eventExecutors)
                 .channel(NioServerSocketChannel.class)
@@ -71,8 +75,8 @@ public class MyRPC {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
                         ChannelPipeline pipeline = ch.pipeline();
-                        pipeline.addLast(new requestDecode());
-                        pipeline.addLast(new requestHandler());
+                        pipeline.addLast(new DecodeHandler());
+                        pipeline.addLast(new ServerHandler());
                     }
                 })
                 .bind(new InetSocketAddress(9090));
@@ -87,14 +91,14 @@ public class MyRPC {
         return (T) Proxy.newProxyInstance(inter.getClassLoader(), new Class<?>[]{inter}, new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                content cont = new content(inter.getName(), method.getName(), method.getParameterTypes(), args);
+                MessageBody cont = new MessageBody(inter.getName(), method.getName(), method.getParameterTypes(), args);
 //                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 //                ObjectOutputStream outputStream = new ObjectOutputStream(byteArrayOutputStream);
 //                outputStream.writeObject(cont);
 //                byte[] body = byteArrayOutputStream.toByteArray();
                 byte[] body = MessageSer.messageEncode(cont);
 
-                header header = createHeader(body);
+                MessageHead header = createHeader(body);
 //                byteArrayOutputStream.reset();
 //                outputStream = new ObjectOutputStream(byteArrayOutputStream);
 //                outputStream.writeObject(header);
@@ -106,7 +110,7 @@ public class MyRPC {
 //                header o = (header)objectInputStream.readObject();
 //                System.out.println(o.toString());
 
-                NioSocketChannel ch = clientFactory.instance.getClient(new InetSocketAddress("localhost", 9090));
+                NioSocketChannel ch = SocketChannelPoolFactory.instance.getClient(new InetSocketAddress("localhost", 9090));
 
                 ByteBuf byteBuf = PooledByteBufAllocator.DEFAULT.directBuffer(body.length + head.length);
                 byteBuf.writeBytes(head);
@@ -120,27 +124,27 @@ public class MyRPC {
 //                });
 //                countDownLatch.await();
                 CompletableFuture<String> cf = new CompletableFuture<>();
-                staticUUID.add(header.requestId, cf);
+                RequestCallback.addCallback(header.requestId, cf);
                 return cf.get();
             }
 
-            private header createHeader(byte[] body) {
+            private MessageHead createHeader(byte[] body) {
                 long uuid = Math.abs(UUID.randomUUID().getLeastSignificantBits());
-                header header = new header(0X14141414, uuid, body.length);
+                MessageHead header = new MessageHead(0X14141414, uuid, body.length);
                 return header;
             }
         });
     }
 }
 
-class staticUUID{
+class RequestCallback {
     private static AtomicInteger repeatNum = new AtomicInteger(0);
     private static ConcurrentHashMap<Long, CompletableFuture> map = new ConcurrentHashMap<Long, CompletableFuture>();
 
-    public static void add(Long requestid, CompletableFuture r){
+    public static void addCallback(Long requestid, CompletableFuture r){
         map.putIfAbsent(requestid, r);
     }
-    public static void run(requestMQ mess){
+    public static void runCallback(UnpackMessage mess){
         CompletableFuture runnable = map.get(mess.head.requestId);
         if(runnable != null){
             runnable.complete(mess.body.result);
@@ -154,38 +158,38 @@ class staticUUID{
     }
 }
 
-class clientPool {
-    NioSocketChannel[] clients;
+class SocketChannelPool {
+    NioSocketChannel[] socketchanns;
     Object[] lock;
 
-    clientPool(int size) {
-        clients = new NioSocketChannel[size];
+    SocketChannelPool(int size) {
+        socketchanns = new NioSocketChannel[size];
         lock = new Object[size];
         Arrays.fill(lock, new Object());
     }
 }
 
-enum  clientFactory{
+enum SocketChannelPoolFactory {
     instance;
 
-    volatile ConcurrentHashMap<InetSocketAddress, clientPool> outboxs = new ConcurrentHashMap<>();
+    volatile ConcurrentHashMap<InetSocketAddress, SocketChannelPool> outboxs = new ConcurrentHashMap<>();
 
     Random random = new Random();
     int poolSize = 10;
 
     public synchronized NioSocketChannel getClient(InetSocketAddress address){
         if (outboxs.get(address) == null){
-            outboxs.put(address, new clientPool(poolSize));
+            outboxs.put(address, new SocketChannelPool(poolSize));
         }
-        clientPool clientPool = outboxs.get(address);
-        int i = random.nextInt(clientPool.clients.length);
-        System.out.println(clientPool.clients.length + " " + Thread.currentThread().getName() + "连接数：" + i);
-        NioSocketChannel cha = clientPool.clients[i];
+        SocketChannelPool clientPool = outboxs.get(address);
+        int i = random.nextInt(clientPool.socketchanns.length);
+        System.out.println(clientPool.socketchanns.length + " " + Thread.currentThread().getName() + "连接数：" + i);
+        NioSocketChannel cha = clientPool.socketchanns[i];
         if (cha != null && cha.isActive()){
             return cha;
         }else {
             NioSocketChannel neety = createNeety(address);
-            clientPool.clients[i] = neety;
+            clientPool.socketchanns[i] = neety;
             return neety;
         }
     }
@@ -199,8 +203,8 @@ enum  clientFactory{
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
                         ChannelPipeline pipeline = ch.pipeline();
-                        pipeline.addLast(new requestDecode());
-                        pipeline.addLast(new readHandler());
+                        pipeline.addLast(new DecodeHandler());
+                        pipeline.addLast(new ClientHandler());
                     }
                 })
                 .connect(address);
@@ -213,29 +217,29 @@ enum  clientFactory{
     }
 }
 
-class requestDecode extends ByteToMessageDecoder{
+class DecodeHandler extends ByteToMessageDecoder{
 
     @Override
     protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf data, List<Object> list) throws Exception {
-        while (data.readableBytes() >= 87){
-            byte[] b = new byte[87];
+        while (data.readableBytes() >= 92){
+            byte[] b = new byte[92];
             data.getBytes(data.readerIndex(), b);
 //            ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(b));
 //            header header = (header) objectInputStream.readObject();
-            header header = MessageSer.messageDecode(b);
+            MessageHead header = MessageSer.messageDecode(b);
 
-            if (data.readableBytes() >= (header.dataLength + 87)){
-                data.readBytes(87);
+            if (data.readableBytes() >= (header.dataLength + 92)){
+                data.readBytes(92);
                 byte[] by  = new byte[(int) header.dataLength];
                 data.readBytes(by);
 //                ObjectInputStream ob = new ObjectInputStream(new ByteArrayInputStream(by));
 //                content body = (content)ob.readObject();
                 if (header.flag == 0x14141414){
-                    content body = MessageSer.messageDecode(by);
-                    list.add(new requestMQ(header, body));
+                    MessageBody body = MessageSer.messageDecode(by);
+                    list.add(new UnpackMessage(header, body));
                 }else if(header.flag == 0x14141424){
-                    content body = MessageSer.messageDecode(by);
-                    list.add(new requestMQ(header, body));
+                    MessageBody body = MessageSer.messageDecode(by);
+                    list.add(new UnpackMessage(header, body));
                 }
             }else {
                 break;
@@ -244,53 +248,61 @@ class requestDecode extends ByteToMessageDecoder{
     }
 }
 
-class requestMQ{
-    header head;
-    content body;
+class UnpackMessage {
+    MessageHead head;
+    MessageBody body;
 
-    public requestMQ(header head, content body) {
+    public UnpackMessage(MessageHead head, MessageBody body) {
         this.head = head;
         this.body = body;
     }
 }
 
-class readHandler extends ChannelInboundHandlerAdapter {
+class ClientHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 //        super.channelRead(ctx, msg);
-        requestMQ byteBuf = (requestMQ) msg;
-        staticUUID.run(byteBuf);
+        UnpackMessage byteBuf = (UnpackMessage) msg;
+        RequestCallback.runCallback(byteBuf);
     }
 }
 
-class requestHandler extends ChannelInboundHandlerAdapter{
+class ServerHandler extends ChannelInboundHandlerAdapter{
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        requestMQ data = (requestMQ) msg;
+        UnpackMessage data = (UnpackMessage) msg;
 
         String name = Thread.currentThread().getName();
         //异步处理逻辑
         ctx.executor().parent().next().execute(()->{
-            content content = new content("io thread:" + name + "   处理逻辑 thread:" + Thread.currentThread().getName() + "      " + data.body.args[0]);
-            byte[] tail = MessageSer.messageEncode(content);
-            header header = new header(0x14141424, data.head.requestId, tail.length);
-            byte[] head = MessageSer.messageEncode(header);
+            Object object = MappingImpl.getMapping(data.body.name);
+            try {
+                Method method = object.getClass().getMethod(data.body.methodname, data.body.methodType);
+                Object invoke = method.invoke(object, data.body.args);
 
-            ByteBuf byteBuf = UnpooledByteBufAllocator.DEFAULT.directBuffer(tail.length + head.length);
-            byteBuf.writeBytes(head);
-            byteBuf.writeBytes(tail);
+                MessageBody content = new MessageBody((String) invoke);
+                byte[] tail = MessageSer.messageEncode(content);
+                MessageHead header = new MessageHead(0x14141424, data.head.requestId, tail.length);
+                byte[] head = MessageSer.messageEncode(header);
 
-            ctx.writeAndFlush(byteBuf);
+                ByteBuf byteBuf = UnpooledByteBufAllocator.DEFAULT.directBuffer(tail.length + head.length);
+                byteBuf.writeBytes(head);
+                byteBuf.writeBytes(tail);
+
+                ctx.writeAndFlush(byteBuf);
+            } catch (Exception e) {
+             e.printStackTrace();
+             }
         });
     }
 }
 
-class header implements Serializable {
+class MessageHead implements Serializable {
     int flag;
     long requestId;
     long dataLength;
 
-    public header(int flag, long requestId, long dataLength) {
+    public MessageHead(int flag, long requestId, long dataLength) {
         this.flag = flag;
         this.requestId = requestId;
         this.dataLength = dataLength;
@@ -306,21 +318,21 @@ class header implements Serializable {
     }
 }
 
-class content implements Serializable {
+class MessageBody implements Serializable {
     String name;
     String methodname;
     Class<?>[] methodType;
     Object[] args;
     String result;
 
-    public content(String name, String methodname, Class<?>[] methodType, Object[] args) {
+    public MessageBody(String name, String methodname, Class<?>[] methodType, Object[] args) {
         this.name = name;
         this.methodname = methodname;
         this.methodType = methodType;
         this.args = args;
     }
 
-    public content(String result) {
+    public MessageBody(String result) {
         this.result = result;
     }
 
@@ -336,10 +348,36 @@ class content implements Serializable {
     }
 }
 
-interface car{
+class MyCar implements Icar {
+    @Override
+    public String hello(String string) {
+        return "ser return: " + string;
+    }
+}
+
+class MyFly implements Ifly {
+    @Override
+    public void hello(String str) {
+        System.out.println("fly ........................");
+    }
+}
+
+class MappingImpl {
+    private static ConcurrentHashMap<String, Object> mappings = new ConcurrentHashMap<>();
+
+    public static void addMapping(String key, Object obj){
+        mappings.putIfAbsent(key, obj);
+    }
+
+    public static Object getMapping(String key){
+        return mappings.get(key);
+    }
+}
+
+interface Icar {
     String hello(String string);
 }
 
-interface fly{
-    void f(String str);
+interface Ifly {
+    void hello(String str);
 }
